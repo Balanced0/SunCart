a964339b62674440b89166d296f21142ae023011
balanced0 <alvichowdhury013@gmail.com>
2026-05-05 12:02:21 +0600

    Made ProductCard, fetched json data and completed the Home section by making is responsive

--- src/app/(Main)/page.js
+++ src/app/(Main)/page.js
@@ -2,11 +2,30 @@ import Image from "next/image";
 import Hero from "../components/Hero";
 import SummerCare from "../components/SummerCare";
 import TopBrands from "../components/TopBrands";
+import ProductCard from "../components/ProductCard";
+import products from "@/data/products.json";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Hero></Hero>
+      <div className="container mx-auto px-4 flex flex-col justify-center items-center">
+        <h3 className="text-center mb-4 pt-10 font-extrabold text-3xl">
+          🔥 Popular Products
+        </h3>
+        <p className="text-center font-medium text-xl text-gray-500 mb-12">
+          Trending summer must-haves
+        </p>
+
+        <div className="mb-10 grid grid-cols-1 gap-6 md:grid-cols-3">
+          {products.slice(0, 3).map((product) => (
+            <ProductCard key={product.id} product={product}></ProductCard>
+          ))}
+        </div>
+
+        <button className="btn btn-lg rounded-xl mb-20"><Link href="/products">View All Products</Link></button>
+      </div>
       <SummerCare></SummerCare>
       <TopBrands></TopBrands>
     </div>
---
+++ src/app/components/ProductCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaStar } from "react-icons/fa6";
+
+const ProductCard = ({product}) => {
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-sm">
+        <figure className="">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-full h-96 object-cover"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{product.name}</h2>
+          <p className="text-gray-500 flex items-center gap-2">{product.rating} <FaStar /></p>
+          <p className="text-orange-500 font-extrabold text-2xl">${product.price}</p>
+          <button className="btn bg-gradient-to-br from-orange-500 to-yellow-400 text-white rounded-xl">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
