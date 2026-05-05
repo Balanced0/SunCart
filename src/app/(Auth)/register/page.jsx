51ccd3b9eb4f10feba40c1c0eb1d059a1f6c7c5c
balanced0 <alvichowdhury013@gmail.com>
2026-05-05 17:56:04 +0600

    Created the connection between MongoDB and Better Auth

--- src/app/(Auth)/register/page.jsx
+++ src/app/(Auth)/register/page.jsx
@@ -1,9 +1,93 @@
+import Link from "next/link";
+
 const RegisterPage = () => {
-    return (
-        <div>
-            RegisterPage
-        </div>
-    );
+  return (
+    <div>
+      <div className="container mx-auto p-4 flex justify-center items-center">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8 mt-20 mb-20">
+          <div>
+            <h3 className="font-extrabold text-3xl text-center mb-3">
+              Create Account
+            </h3>
+            <p className="font-medium text-base text-center text-gray-400 mb-4">
+              Join SunCart for exclusive summer deals
+            </p>
+          </div>
+
+          <label className="label text-black text-base font-bold">Name</label>
+          <input
+            type="text"
+            className="input mb-4 w-full rounded-xl"
+            placeholder="Enter your name"
+          />
+
+          <label className="label text-black text-base font-bold">Email</label>
+          <input
+            type="email"
+            className="input mb-4 w-full rounded-xl"
+            placeholder="Enter your Email"
+          />
+
+          <label className="label text-black text-base font-bold">
+            Photo URL
+          </label>
+          <input
+            type="text"
+            className="input mb-4 w-full rounded-xl"
+            placeholder="Enter URL"
+          />
+
+          <label className="label text-black text-base font-bold">
+            Password
+          </label>
+          <input
+            type="password"
+            className="input w-full rounded-xl"
+            placeholder="Enter Password"
+          />
+
+          <button className="btn btn-lg bg-gradient-to-br from-orange-500 to-yellow-400 text-white font-bold rounded-xl mt-4">
+            Login
+          </button>
+
+          <div className="divider">OR</div>
+
+          <button className="btn rounded-xl bg-white text-black border-[#e5e5e5]">
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+            Login with Google
+          </button>
+
+          <h3 className="text-center text-sm"><span className="text-gray-500">Already have an account?</span> <Link href="/login" className="text-orange-500">Login</Link> </h3>
+        </fieldset>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
