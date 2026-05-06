90f9ff3a1a7d19ad1dbbc812831895e3930e72f1
balanced0 <alvichowdhury013@gmail.com>
2026-05-06 21:46:47 +0600

    Refactor ProductCard to handle navigation with session check on button click

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,8 +1,23 @@
+"use client"
 import Link from "next/link";
 import React from "react";
 import { FaStar } from "react-icons/fa6";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from 'next/navigation'
 
 const ProductCard = ({product}) => {
+  const { data: session } = authClient.useSession()
+
+  const router = useRouter()
+
+  const handleViewDetails = () =>{
+    if(session){
+      router.push(`/productPage/${product.id}`)
+    }
+    else{
+      router.push('/login')
+    }
+  }
   return (
     <div>
       <div className="card bg-base-100 shadow-sm">
@@ -17,7 +32,7 @@ const ProductCard = ({product}) => {
           <h2 className="card-title">{product.name}</h2>
           <p className="text-gray-500 flex items-center gap-2">{product.rating} <FaStar /></p>
           <p className="text-orange-500 font-extrabold text-2xl">${product.price}</p>
-          <Link href={`/productPage/${product.id}`}><button className="btn bg-gradient-to-br from-orange-500 to-yellow-400 text-white rounded-xl w-full">View Details</button></Link>
+          <button onClick={handleViewDetails} className="btn bg-gradient-to-br from-orange-500 to-yellow-400 text-white rounded-xl w-full">View Details</button>
         </div>
       </div>
     </div>
