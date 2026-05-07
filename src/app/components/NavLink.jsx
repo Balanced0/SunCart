0dbc494f0da6d2901a17bbb7c3358e2fd580b600
balanced0 <alvichowdhury013@gmail.com>
2026-05-07 13:25:23 +0600

    Fixed some bugs in the NavLink

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -7,9 +7,7 @@ const NavLink = ({href, children}) => {
     const path = usePathname();
     const isActive = href === path;
     return (
-        <div>
-            <Link href={href} className={isActive? "text-orange-400 font-bold":"text-gray-500 font-bold"}>{children}</Link>
-        </div>
+        <Link href={href} className={isActive? "text-orange-400 font-bold":"text-gray-500 font-bold"}>{children}</Link>
     );
 };
 
